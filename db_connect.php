f086f64900227e6ad329f3ed97da8820b334573c
= <teivafr@gmail.com>
2023-01-30 17:19:43 +0100

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,6 @@
 <?php
-	$json = $_ENV['credentials-postgre'];
-	$data = json_decode($json, true);
-	$password = $data['password'];
-	$username = $data['username'];
-
+	$password = $_ENV['password'];
+	$username = $_ENV['username'];
 	$server = "servsqltf-tp.postgres.database.azure.com";
 	$db = "pg_teiva_francis";
 	$conn = pg_connect("host=$server dbname=$db user=$username password=$password");
