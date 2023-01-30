f5e4c04af7ee8f0d62168d8401634bf51d566f2d
= <teivafr@gmail.com>
2023-01-31 00:59:05 +0100

    ddddd

--- db_connect.php
+++ db_connect.php
@@ -2,6 +2,6 @@
 	$server = $_ENV['host'];
 	$username = $_ENV['user'];
 	$password = $_ENV['password'];
-	$db = "pg_teiva_francis";
+	$db = "pg_teiva_francis";  
 	$conn = pg_connect("host=$server dbname=$db user=$username password=$password");
 ?>
