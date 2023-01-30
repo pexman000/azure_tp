c01f0681308d1844a63ba7d3675b09e18e61f5e1
= <teivafr@gmail.com>
2023-01-30 09:55:11 +0100

    commit final

--- index.php
+++ index.php
@@ -1,42 +1,33 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Liste des acteurs</title>
-	<style>
-		table {
-			border-collapse: collapse;
-			width: 100%;
-		}
-		th, td {
-			border: 1px solid black;
-			padding: 8px;
-			text-align: left;
-		}
-		th {
-			background-color: #f2f2f2;
-		}
-	</style>
-</head>
-<body>
-	<h1>Liste des acteurs</h1>
-	<table>
-		<tr>
-			<th>Id</th>
-			<th>Prénom</th>
-			<th>Nom</th>
-		</tr>
-		<!-- PHP code to fetch and display data from API -->
-		<?php
-			$data = file_get_contents("http://localhost/api/actors.php");
-			$actors = json_decode($data, true);
-			foreach ($actors as $actor) {
-				echo "<tr>";
-				echo "<td>" . $actor['Id'] . "</td>";
-				echo "<td>" . $actor['Prénom'] . "</td>";
-				echo "<td>" . $actor['Nom'] . "</td>";
-				echo "</tr>";
-			}
-		?>
-	</table>
-</body>
-</html>
+<?php
+	// Connect to database
+	include("db_connect.php");
+	$request_method = $_SERVER["REQUEST_METHOD"];
+
+	function getActors()
+    {
+        global $conn;
+        $result = pg_query($conn, "SELECT * FROM actor");
+        if (!$result) {
+            echo json_encode(array("error" => "An error occurred."));
+            exit;
+        }
+        $response = array();
+        while ($row = pg_fetch_row($result)) {
+            $response[] = array("Id" => $row[0], "Prénom" => $row[1], "Nom" => $row[1]);
+        }
+        header('Content-Type: application/json');
+        echo json_encode($response, JSON_PRETTY_PRINT);
+    }
+	
+	switch($request_method)
+	{
+		
+		case 'GET':
+            getActors();
+			break;
+		default:
+			header("HTTP/1.0 405 Method Not Allowed");
+			break;
+            
+	}
+?>
